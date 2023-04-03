d3da6cebd6d4a04c55553ccb2c1e782043460d99
Rohit Kakade <92684754+rohitkakade08@users.noreply.github.com>
2023-04-03 20:18:01 +0530

    Add files via upload

---
+++ TXON_03-main/README.md
@@ -0,0 +1,4 @@
+This is Task-02 of the internship.
+##Quiz Android Application Using JAVA -It is a basic Quiz App developed using Java.
+
+-Relative Layout is used for design.
---
+++ TXON_03-main/app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Which Company Owns the Android?",
+            "Which is not a programming language?",
+            "'OS' computer abbreviation usually means?",
+            "Who is the owner of Apple Company?",
+            ".mov extension refers usually to what kind of file?"
+    };
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Order of Significance","Open Software","Optical Sensor","Operating System"},
+            {"Steve Jobs","Elon Musk","Ratan Tata","Bill Gates"},
+            {"Image file","Animation file","Audio File","MS Office Document"}
+
+    };
+
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "Operating System",
+            "Steve Jobs",
+            "Animation File"
+
+    };
+}
